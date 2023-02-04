265f006427facc9e4a9721e53ecf0d0e0c31045e
Rio <rio.lim@creatory.id>
2023-02-04 15:15:33 +0700

    add config for env of development

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@
 const withLess = require("next-with-less");
 
 module.exports = withLess({
+  env: {
+    APP_ENV: process.env.APP_ENV,
+  },
   images: {
     domains: ["images.unsplash.com"],
   },
